031193b468abb972c357bad822ba8d818050c03b
rjayaa <rjaya.lelana03@gmail.com>
2023-03-05 16:16:44 +0700

    UI Form Buku done

---
+++ AoLv2/AoLv2/2InsertBuku.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AoLv2
+{
+    public partial class _2InsertBuku : Form
+    {
+        public _2InsertBuku()
+        {
+            InitializeComponent();
+        }
+    }
+}
